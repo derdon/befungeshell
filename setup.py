dfefce4efac7217448681eae8bfc84b1918ed383
derdon <liedtke.simon@googlemail.com>
2011-07-27 17:20:44 +0200

    use setuptools, if available, and add the ordereddict requirement if it is needed

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from __future__ import with_statement
 
-from distutils.core import setup
+import sys
+
+try:
+    from setuptools import setup
+    with_setuptools = True
+except ImportError:
+    from distutils.core import setup
+    with_setuptools = False
 
 with open('README.rst') as f:
     readme_content = f.read()
+
+# version 2.5, 2.6 and 3.0 need the 3rd party package ordereddict
+py_version = sys.version_info[:2]
+needs_ordereddict = py_version in set([(2, 5), (2, 6), (3, 0)])
+
+extra = {}
+if with_setuptools:
+    extra['install_requires'] = needs_ordereddict
 
 setup(
     name='befungeshell',
@@ -33,4 +48,5 @@ setup(
         'Topic :: Software Development'],
     py_modules=['befunge_shell'],
     scripts=['befunge_shell.py'],
+    **extra
 )
